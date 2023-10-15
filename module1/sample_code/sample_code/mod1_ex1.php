0d58b63a72acdf7aaabba38dac4c03e6171240c6
Jonathan Mullet <jmullet12100@gmail.com>
2023-10-14 21:21:38 -0700

    Initial commit

---
+++ module1/sample_code/sample_code/mod1_ex1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>My First PHP Page</title>
+    </head>
+    <body>
+
+        <?php
+            echo "This is an example of a PHP script!";
+        ?>
+
+    </body>
+</html>
